a490ee64d9b7436fe0eaa7e56cb93b8c918b5af6
khan-shahnawaz-09 <khan.dev006@gmail.com>
2025-06-29 11:07:08 +0530

    Add Java fundamentals code

---
+++ 01-variables/input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+    
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println("number is "+(a+b));
+        
+    }
+}
